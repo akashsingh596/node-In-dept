c86a42da33d114466da6bfa51610b3dedf1dbe45
akashsingh596 <72357922+akashsingh596@users.noreply.github.com>
2025-02-16 22:56:44 +0530

    server rendering

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const http = require('http')
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
 
 const server = http.createServer((req, res) => {
-    console.log("new request")
-    res.end("Hello World")
-})
+//   res.end("Hello World");
+  const log = `${Date.now()} : ${req.url} New Request Recived\n`;
+  const myUrl = url.parse(req.url, true)
+  fs.appendFile("log.txt", log, (err ,data) => {
+    switch(myUrl.pathname){
+        case "/": res.end("Hello World");
+            break;
+        case "/about":res.end("About Page");
+            break;
+        default: res.end("404 Page Not Found");
+    }
+  });
+});
 
-server.listen(3000,()=>{
-    console.log("Server is running on port 3000")
-})
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
